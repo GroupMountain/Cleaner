1fef24beedeee62c87f6ad78f30a5e605b03d94c
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2024-05-03 00:43:13 +0800

    adapt: adapt to GMLIB v0.12.4

--- src/Features/Helper.cpp
+++ src/Features/Helper.cpp
@@ -2,8 +2,12 @@
 
 namespace Helper {
 
-void broadcastMessage(std::string_view msg) { GMLIB_Level::broadcast(I18nAPI::get("cleaner.info.prefix") + std::string(msg)); }
+void broadcastMessage(std::string_view msg) {
+    GMLIB_Level::getInstance()->broadcast(I18nAPI::get("cleaner.info.prefix") + std::string(msg));
+}
 
-void broadcastToast(std::string_view msg) { GMLIB_Level::broadcastToast(I18nAPI::get("cleaner.info.prefix"), msg); }
+void broadcastToast(std::string_view msg) {
+    GMLIB_Level::getInstance()->broadcastToast(I18nAPI::get("cleaner.info.prefix"), msg);
+}
 
 } // namespace Helper
